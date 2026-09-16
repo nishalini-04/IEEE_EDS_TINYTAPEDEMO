7981ae920e4199e9b291f4b5a4a5b11cff472a5c
nishalini-04 <nishalini487@gmail.com>
2026-09-16 12:09:12 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,6 +1,3 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
@@ -8,33 +5,57 @@ from cocotb.triggers import ClockCycles
 
 @cocotb.test()
 async def test_project(dut):
-    dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
+    dut._log.info("Starting Half Adder test")
+
+    # Start clock
     clock = Clock(dut.clk, 10, unit="us")
     cocotb.start_soon(clock.start())
 
-    # Reset
-    dut._log.info("Reset")
+    # Initial values
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+
+    # Reset
+    await ClockCycles(dut.clk, 2)
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
-
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
+    # --------------------------------
+    # A = 0, B = 0
+    # Expected: SUM = 0, CARRY = 0
+    # --------------------------------
+    dut.ui_in.value = 0b00
     await ClockCycles(dut.clk, 1)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    assert (int(dut.uo_out.value) & 0b11) == 0b00
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    # --------------------------------
+    # A = 0, B = 1
+    # Expected: SUM = 1, CARRY = 0
+    # --------------------------------
+    dut.ui_in.value = 0b01
+    await ClockCycles(dut.clk, 1)
+
+    assert (int(dut.uo_out.value) & 0b11) == 0b01
+
+    # --------------------------------
+    # A = 1, B = 0
+    # Expected: SUM = 1, CARRY = 0
+    # --------------------------------
+    dut.ui_in.value = 0b10
+    await ClockCycles(dut.clk, 1)
+
+    assert (int(dut.uo_out.value) & 0b11) == 0b01
+
+    # --------------------------------
+    # A = 1, B = 1
+    # Expected: SUM = 0, CARRY = 1
+    # --------------------------------
+    dut.ui_in.value = 0b11
+    await ClockCycles(dut.clk, 1)
+
+    assert (int(dut.uo_out.value) & 0b11) == 0b10
+
+    dut._log.info("HALF ADDER TEST PASSED")
